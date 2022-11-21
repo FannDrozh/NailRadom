4b6db66ad8d6d12ff23aae78938783040ffc852a
unknown <@ngknn.local>
2022-11-21 13:55:56 +0300

    Initial commit

---
+++ app/src/main/java/com/example/nailradom/NailMain.java
@@ -0,0 +1,14 @@
+package com.example.nailradom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NailMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nail_main);
+    }
+}
