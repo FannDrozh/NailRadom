134e98536adeb9e88281192428c2daaafbd0795e
FannDrozh <ya.ilya-03@ya.ru>
2022-11-21 14:53:37 +0300

    0.1

---
+++ app/src/main/java/com/example/nailradom/SettingsNail.java
@@ -0,0 +1,14 @@
+package com.example.nailradom;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsNail extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_nail);
+    }
+}
