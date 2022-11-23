3d8e92acf3d782606661da56afe1aedcb7cf1c27
БарашенковИТ <@105-07>
2022-11-23 13:21:02 +0300

    0.2

--- app/src/main/java/com/example/nailradom/FinalNail.java
+++ app/src/main/java/com/example/nailradom/FinalNail.java
@@ -3,15 +3,12 @@ package com.example.nailradom;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.view.View;
 
-public class NailMain extends AppCompatActivity {
+public class FinalNail extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_nail_main);
+        setContentView(R.layout.activity_final_nail);
     }
-
-
 }
